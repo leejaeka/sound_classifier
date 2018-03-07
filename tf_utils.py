f31df06b5171e417cdbd1d2a8d40de72565d8189
Liev Garcia <liev.garcia@gmail.com>
2018-03-07 11:56:56 +0000

    something runs

--- tf_utils.py
+++ tf_utils.py
@@ -5,7 +5,7 @@ import math
 import glob
 from sklearn.model_selection import train_test_split
 
-def load_data():
+def load_features():
     filelist = glob.glob('features_mel_spectrograms/*.npy')
     labels = []
     data = []
@@ -15,14 +15,15 @@ def load_data():
         if 'cat' in file:
             label = 0 #'cat'
         else:
-            label = 1 #'dog'        
-        crop = int(nfile.shape[1] / 29)
+            label = 1 #'dog'
+        crop = int(nfile.shape[1] / 28)
         for i in range(crop):
             labels.append(label)
-            data.append(nfile[:,i*29:(i+1)*29])        
+            data.append(nfile[:,i*28:(i+1)*28])
 
     X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.1, random_state=59)
-    return np.array(X_train), np.array(X_test), np.array(y_train), np.array(y_test)
+    # return X_train, X_test, y_train, y_test
+    return np.array(X_train, dtype=np.float32), np.array(X_test, dtype=np.float32), np.array(y_train), np.array(y_test)
 
 def load_dataset():
     train_dataset = h5py.File('datasets/train_signs.h5', "r")
@@ -34,31 +35,31 @@ def load_dataset():
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 
     classes = np.array(test_dataset["list_classes"][:]) # the list of classes
-    
+
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
-    
+
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
 
 def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
     """
     Creates a list of random minibatches from (X, Y)
-    
+
     Arguments:
     X -- input data, of shape (input size, number of examples)
     Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
     mini_batch_size - size of the mini-batches, integer
     seed -- this is only for the purpose of grading, so that you're "random minibatches are the same as ours.
-    
+
     Returns:
     mini_batches -- list of synchronous (mini_batch_X, mini_batch_Y)
     """
-    
+
     m = X.shape[1]                  # number of training examples
     mini_batches = []
     np.random.seed(seed)
-    
+
     # Step 1: Shuffle (X, Y)
     permutation = list(np.random.permutation(m))
     shuffled_X = X[:, permutation]
@@ -71,14 +72,14 @@ def random_mini_batches(X, Y, mini_batch_size = 64, seed = 0):
         mini_batch_Y = shuffled_Y[:, k * mini_batch_size : k * mini_batch_size + mini_batch_size]
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
-    
+
     # Handling the end case (last mini-batch < mini_batch_size)
     if m % mini_batch_size != 0:
         mini_batch_X = shuffled_X[:, num_complete_minibatches * mini_batch_size : m]
         mini_batch_Y = shuffled_Y[:, num_complete_minibatches * mini_batch_size : m]
         mini_batch = (mini_batch_X, mini_batch_Y)
         mini_batches.append(mini_batch)
-    
+
     return mini_batches
 
 def convert_to_one_hot(Y, C):
@@ -87,35 +88,35 @@ def convert_to_one_hot(Y, C):
 
 
 def predict(X, parameters):
-    
+
     W1 = tf.convert_to_tensor(parameters["W1"])
     b1 = tf.convert_to_tensor(parameters["b1"])
     W2 = tf.convert_to_tensor(parameters["W2"])
     b2 = tf.convert_to_tensor(parameters["b2"])
     W3 = tf.convert_to_tensor(parameters["W3"])
     b3 = tf.convert_to_tensor(parameters["b3"])
-    
+
     params = {"W1": W1,
               "b1": b1,
               "W2": W2,
               "b2": b2,
               "W3": W3,
               "b3": b3}
-    
+
     x = tf.placeholder("float", [12288, 1])
-    
+
     z3 = forward_propagation_for_predict(x, params)
     p = tf.argmax(z3)
-    
+
     sess = tf.Session()
     prediction = sess.run(p, feed_dict = {x: X})
-        
+
     return prediction
 
 def forward_propagation_for_predict(X, parameters):
     """
     Implements the forward propagation for the model: LINEAR -> RELU -> LINEAR -> RELU -> LINEAR -> SOFTMAX
-    
+
     Arguments:
     X -- input dataset placeholder, of shape (input size, number of examples)
     parameters -- python dictionary containing your parameters "W1", "b1", "W2", "b2", "W3", "b3"
@@ -124,20 +125,19 @@ def forward_propagation_for_predict(X, parameters):
     Returns:
     Z3 -- the output of the last LINEAR unit
     """
-    
-    # Retrieve the parameters from the dictionary "parameters" 
+
+    # Retrieve the parameters from the dictionary "parameters"
     W1 = parameters['W1']
     b1 = parameters['b1']
     W2 = parameters['W2']
     b2 = parameters['b2']
     W3 = parameters['W3']
-    b3 = parameters['b3'] 
+    b3 = parameters['b3']
                                                            # Numpy Equivalents:
     Z1 = tf.add(tf.matmul(W1, X), b1)                      # Z1 = np.dot(W1, X) + b1
     A1 = tf.nn.relu(Z1)                                    # A1 = relu(Z1)
     Z2 = tf.add(tf.matmul(W2, A1), b2)                     # Z2 = np.dot(W2, a1) + b2
     A2 = tf.nn.relu(Z2)                                    # A2 = relu(Z2)
     Z3 = tf.add(tf.matmul(W3, A2), b3)                     # Z3 = np.dot(W3,Z2) + b3
-    
+
     return Z3
-    
